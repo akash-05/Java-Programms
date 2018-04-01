2420702bbf21bea37450802d393bdbfc78c944d0
akash <akashkumarchaubey@gmail.com>
2018-04-01 22:34:06 +0530

    first commit

---
+++ src/PrimeNumber.java
@@ -0,0 +1,24 @@
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		PrimeNumber pn = new PrimeNumber();
+		System.out.println(pn.prime(2));
+
+	}
+
+	public boolean prime(int n) {
+		boolean result = true;
+		for (int i = 2; i < n / 2; i++) {
+			if (n % i == 0) {
+				return false;
+			} else {
+				result = true;
+			}
+		}
+
+		return result;
+
+	}
+
+}
